ddf8e01c350b1f018a63d1b48ceca09c9da738ca
chavalk <chavalk@hotmail.com>
2021-07-09 19:55:40 -0500

    Move JavaScript code to separate js file and optimize code.

---
+++ script.js
@@ -0,0 +1,36 @@
+// Add current day
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+// Function to determine color code
+function colorCode() {
+    var currentTime = moment().hour();
+    var time = 9;
+    for (let i = 9; i < 18; i++) {
+        if (time < currentTime) {
+            $("#" + i + "hr").addClass("past");
+        } else if (time == currentTime) {
+            $("#" + i + "hr").addClass("present");
+        } else if (time > currentTime) {
+            $("#" + i + "hr").addClass("future");
+        }
+        time++;
+    }
+}
+
+// Function to load items from local storage
+function loadSavedItems() {
+    for (let i = 9; i < 18; i++) {
+        $("#" + i + "hr").text(localStorage.getItem(i + "h"));
+    }
+}
+
+// Save entry to local storage
+$(".saveBtn").on("click", function() {
+    localStorage.setItem($(this).parent().attr("id"), $("#9hr").val());
+})
+
+// Call function to color code
+colorCode();
+
+// Call function to load saved items
+loadSavedItems();
